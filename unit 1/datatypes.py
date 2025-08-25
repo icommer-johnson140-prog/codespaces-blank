ad669bd4afb23eddd07dc90e09394ed9fef911d1
icommer-johnson140-prog <icommer-johnson140@boyslatin.net>
2025-08-25 12:35:23 +0000

    notes for 8-25-25

--- unit 1/datatypes.py
+++ unit 1/datatypes.py
@@ -1,4 +1,4 @@
-"This a string
+"This a string - characters wrapped in quotations
 "Isaiah Commer-Johnson"
 "19122
 1600
@@ -7,3 +7,16 @@
 1800
 # this is a message only humans can see this is a comment.
 # it is invisible to the computer
+# intergers - number values that can only be whole numbers
+# floats - number values that can only be decimal numbers
+10.10
+23.333444
+0.123521
+# boolean - is a value that can be either true or fulse
+true
+false
+0 
+1
+# variables - containers for data
+school name = "boys latin
+phone = "2155942832"
