1895dbec4638eeae51d27935f662ce6145ba2819
icommer-johnson140-prog <icommer-johnson140@boyslatin.net>
2025-09-02 12:48:26 +0000

    notes sept 2nd

---
+++ builtinfinction.py
@@ -0,0 +1,18 @@
+# built in functions - pre written code that is stored
+# in the programming language. so long as you know the 
+# keyword you can use its power
+
+# Float() - allows us to change whole numbers decimal
+priceofDrink = 10
+nameofdrink = water
+
+
+print(float(price of drink))
+
+
+
+# print ()
+
+
+
+# input () - allows us to change decimals into whole numbers
--- unit 1/operators.py
+++ unit 1/operators.py
@@ -2,4 +2,23 @@
 # data types more actions and power
 # Arithmetic - math 
 # arithmetic operators are just math operations
+
+# Arithmetic , operators, additio
+# multiolifstion, and dvidon symbols
 print(2+56)
+value +
+# assignment operators - passign data variable
+# comparison - comparing values
+print (Student1 > StudentB
+chickenthighs = 10.00
+macandcheese = 15.00
+qb = 'jr'
+wr1 = 'zay'
+wr2 = 'cash'
+te = 'janir'
+alegebra = true 
+literature = True
+print (algebra == true and literature == False)
+# and only returns true if both conditions is true it will
+# return true
+
