7cfed600902e0056f0e78bfddeac86bfaffc1122
icommer-johnson140-prog <icommer-johnson140@boyslatin.net>
2025-09-03 12:48:45 +0000

    9-3-25

--- builtinfinction.py
+++ builtinfinction.py
@@ -16,3 +16,23 @@ print(float(price of drink))
 
 
 # input () - allows us to change decimals into whole numbers
+
+print ('gg')
+
+input("what time is it?")
+
+print('what time is it?')
+
+#input() function - this is a built in function that allows users to
+# type data into a program and assign it to a variable. The newly
+#inputted data will always be treated as a string
+3 INSTRUCTIONS:
+#choose the correct data casting function to fix the following questions 
+
+#problem 1
+Name = input ('what is your name')
+Age = input ('what is your age')
+
+# problem 2 
+Value = true
+print(true = value)
