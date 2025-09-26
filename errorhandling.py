1af14cbdc7723e13990f26094474f88d19f049ca
icommer-johnson140-prog <icommer-johnson140@boyslatin.net>
2025-09-26 12:48:21 +0000

    quiz

--- errorhandling.py
+++ errorhandling.py
@@ -1,3 +1,3 @@
 # type error - computer detected data types being used that werent the same
 
-# syntax error - 
+# syntax error - a character or string incorrectly placed in a command or instruction that causes a failure in execution.
---
+++ unit 1/conditions.py
@@ -0,0 +1,17 @@
+# conditional Statement syntax
+# if keyword - controls the condition we want to satisfy
+
+# else keyword- our default outcome. the thing that
+# happens when our conditions are not satisfied
+
+    studentcredit = input ("please enter number of credits you have")
+
+
+if int(student credit)>=50:
+    print ("congrats! you are eligible to graduate.")
+else:
+    print("sorry, you hvae not satisfied the required credit points.")
+we 
+
+
+if int(): 
