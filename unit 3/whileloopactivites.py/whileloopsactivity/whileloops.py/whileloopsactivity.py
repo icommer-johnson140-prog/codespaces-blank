5f9745b44b73536df03ee64d321b28a559feda92
icommer-johnson140-prog <icommer-johnson140@boyslatin.net>
2025-10-30 12:47:58 +0000

    10-30-25

---
+++ unit 3/whileloopactivites.py/whileloopsactivity/whileloops.py/whileloopsactivity.py
@@ -0,0 +1,91 @@
+i = 1
+while i < 5:
+    print("here are some instructions")
+    print
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# create function
+# print out a list
+# when user type quit stop loop
+
+
+#def Listloop():
+#   numbers = []
+ #   userIput = input ('please stop in a number')
+  # newNumber = int(userInput)
+   # numbers.append(newNumber)
+    #print(numbers)
+
+#else:
+#print('loop has stopped')
+#listloop()
+
+
+
+
+
+
+
+# create function 
+# when a user confirm they done entering a number
+# add the numbers up and print out the result , your function
+# should use while loop where so long as user
+# do not enter "quit", it should continue adding numbers
+
+
+def Listloop()
+    number = []
+    userInput = input ('continue to add number')
+    else int(user say "quit")
+    newNumber = int(userInput)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def guessingLoop()
+    correctNumber = 7
+    guess = input("please type in a number")
+    while guess != correctNumber:
+        print(number is to high)
+        guess = int(input("please type in a number"))
+    else:
+        print("number is to low")
+        guess = int(input("please type in a number"))
+
+        print("congrtas that is correct!")
+    
+    
+    else:
+           print("congrtas that is correct!")
+    
+
+
+
+guessingLoop()        
---
+++ unit 3/whileloops.py
@@ -0,0 +1,26 @@
+# while loop definition - a while loop is a type is a construct
+# where code instructions will keep running so 
+# long as a condition is true (boolean)
+
+# while loop syntax
+
+#videoGameAge = 17
+#purchaserAge = int(input('how old are you?'))
+
+#while videoGameAge >= purchaserAge: # statement is true
+# (17>15)
+   # print('you are not old enough to buy this game')
+#else:
+    # print('cool, you can buy this game')
+
+
+savedPw = '123abc'
+userpw = input("please enter your pw:")
+
+
+while savedPw = userpw
+    print('incorrect pw try again')
+    userpw = input('please type your password again')
+    if attempt ==3:
+else:
+    print("welcome to your account")
