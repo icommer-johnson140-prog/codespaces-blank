32401ab6ab77c0491d41d97d48350c6afaf47109
icommer-johnson140-prog <icommer-johnson140@boyslatin.net>
2025-12-03 13:48:24 +0000

    12-3-25

---
+++ projects/miniproject1.py
@@ -0,0 +1,16 @@
+def isaiahRandomQuiz():
+print(" what is 21")
+print("A. 16")
+print("B. 27")
+print("C. 18")
+print("d. 35")
+
+answer1 = input('user answer:')
+print(answer1)
+
+correctAnswer_1 = 'B'
+
+if userAnswer_ == correctAnswer_1:
+    print('correct')
+else:
+    print('incorrect')
