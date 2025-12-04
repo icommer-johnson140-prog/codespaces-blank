0229977c17c0e70780ee1c0830ccbf67b8782ccb
icommer-johnson140-prog <icommer-johnson140@boyslatin.net>
2025-12-04 13:49:02 +0000

    12-4-25

--- projects/miniproject1.py
+++ projects/miniproject1.py
@@ -1,5 +1,5 @@
 def isaiahRandomQuiz():
-print(" what is 21")
+    print(" what does 6 + 21 =")
 print("A. 16")
 print("B. 27")
 print("C. 18")
@@ -12,5 +12,68 @@ correctAnswer_1 = 'B'
 
 if userAnswer_ == correctAnswer_1:
     print('correct')
-else:
+if userAnswer != correctAnswer_1:
     print('incorrect')
+
+
+
+
+
+
+
+
+
+
+    print("what are BL school colors?")
+print("A. red and navy blue")
+print("B. maroon and grey")
+print("C. Navy blue and maroon")
+print("D. Grey and red")
+
+answer1 = input('user answer:')
+print(answer1)
+
+
+correctAnswer_1 = 'C'
+
+if userAnswer == correctAnswer_1:
+    print("correct")
+
+else:
+    print("incorrect")
+    
+    if userAnswer != correctAnswer_1:
+        print("incorrect")
+
+
+
+
+
+
+print('who was the rushing leader for the 2024-2025 nfl season?')
+print("A. Saquon barkley")
+print("B. josh jacobs")
+print("C. johnathan taylor")
+print("D. derrick henry")
+
+answer1 = input('user answer:')
+print(answer1)
+
+correctAnswer_1 = 'A'
+
+if userAnswer_ == correctAnswer_1:
+    print('correct')
+if userAnswer != correctAnswer_1:
+    print('incorrect')
+
+
+
+
+
+
+
+if userAnswers = 'B,C,A'
+print("Congrats you earned a perfect score of 3/3")
+
+if userAnswer = 'D,C,A'
+print("you earned a score of 2/3")
