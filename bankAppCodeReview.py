74bde4e218390c90a20d7400465147a2612cca9c
icommer-johnson140-prog <icommer-johnson140@boyslatin.net>
2026-01-14 13:20:01 +0000

    1-13-26

---
+++ bankAppCodeReview.py
@@ -0,0 +1,22 @@
+# 1. write code that will put 25% of paycheck into the savingsAccount variable 
+# 2. write code that will put 25% of paycheck into retirementAccount variable
+# 3. write code that will put the remaining 50% of the paycheck into the checkingsAccount variable
+# 4. Print what the user will have in each account
+
+
+
+def payCheckFilter(payRate,hours,daysworked):
+    checkingAccount = 0
+    retirementAccount = 0
+    savingsAccount = 0
+    paycheck = payRate * hours * daysworked
+    print('My pay check for working.'+str(daysworked) + 'day(s) will be $' + str(paycheck))
+
+    checkingAccount += paycheck *.5
+    retirementAccount += paycheck *.25
+    SavingsAccount += paycheck *.25
+print('My pay check'+ str (daysWorked)+ ')
+payCheckFilter(45.00,8,5)
+
+
+
